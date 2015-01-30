77f899b36840c117479d93137c60e0d16f947b59
gregpachman <gregpachman@gmail.com>
2015-01-30 15:32:02 -0500

    Assorted Classroom Work

--- WarCardGame/WarCardGame/ViewController.swift
+++ WarCardGame/WarCardGame/ViewController.swift
@@ -15,8 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var fightButton: UIButton!
     
+    @IBOutlet weak var leftCardScoreboard: UITextField!
     
+    @IBOutlet weak var playAgain: UIButton!
     
+    @IBOutlet weak var rightCardScoreboard: UITextField!
+    
+    var leftScoreTotal = 0
+    var rightScoreTotal = 0
     
     
     override func viewDidLoad() {
@@ -32,19 +38,45 @@ class ViewController: UIViewController {
 
     @IBAction func fightButtonTap(sender: AnyObject) {
         
-        var leftRandomCard = arc4random_uniform(13) + 2
+        var leftRandomCard = arc4random_uniform(14) + 2
         var leftCardResult: String = String(format: "%i", leftRandomCard)
         
         
-        var rightRandomCard = arc4random_uniform(13) + 2
+        var rightRandomCard = arc4random_uniform(14) + 2
         var rightCardResult: String = String(format: "%i", rightRandomCard)
         
         self.leftCardView.image = UIImage(named: leftCardResult)
         self.rightCardView.image = UIImage(named: rightCardResult)
         
+
+    
+    if leftRandomCard > rightRandomCard {
+    
+            leftScoreTotal += 1
+            self.leftCardScoreboard.text = String(leftScoreTotal)
         
     }
-    
-    
+        else if leftRandomCard == rightRandomCard{
+            
+        
+        }else {
+            rightScoreTotal += 1
+            self.rightCardScoreboard.text = String(rightScoreTotal)
 }
-
+        
+        if rightScoreTotal > 9 || leftScoreTotal > 9 {
+            
+            fightButton.hidden = true
+            playAgain.hidden = false
+        }
+}
+    @IBAction func playAgainButtonTap(sender: AnyObject) {
+        
+        playAgain.hidden = true
+        leftCardScoreboard.text = "0"
+        rightCardScoreboard.text = "0"
+        leftScoreTotal = 0
+        rightScoreTotal = 0
+        fightButton.hidden = false
+    }
+}
